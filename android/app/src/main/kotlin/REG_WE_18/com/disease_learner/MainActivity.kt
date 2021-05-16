90b98aea7602cffb3e866aa6d19da50e433d3883
Rashini <it18113150@my.sliit.lk>
2021-05-16 19:57:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/REG_WE_18/com/disease_learner/MainActivity.kt
@@ -0,0 +1,6 @@
+package REG_WE_18.com.disease_learner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/Common/NavDrawer.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+//import 'package:disease_learner/Screens/Admin/AddDisease.dart';
+
+
+
+
+class NavDrawer extends StatefulWidget {
+  @override
+  NavDrawer({Key key, this.title}) : super(key: key);
+  final String title;
+  _NavDrawerD createState() => _NavDrawerD();
+
+
+}
+class _NavDrawerD extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+          color: Color.fromRGBO(35,58,98,1),
+        child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 100.0,
+          child: DrawerHeader(
+
+            child: Text(
+              'MANUJAYA' ,
+              style: TextStyle(color: Color.fromRGBO(245, 229, 186, 1), fontSize: 20),
+            ),
+          ),
+//            decoration: BoxDecoration(
+//                color: Colors.green,
+//                image: DecorationImage(
+//                    fit: BoxFit.fill,
+//                    )),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text('Add Disease' , style: const TextStyle(
+                color: Color.fromRGBO(254, 246, 222, 1),
+              fontSize: 20
+            ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+//              Navigator.push(
+//                  context, MaterialPageRoute(builder: (context) => AddDiseases())
+//              );
+            },
+          ),
+
+
+
+
+
+
+       ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Edit/Delete Diseases',style: const TextStyle(
+                color: Color.fromRGBO(254, 246, 222, 1),
+                fontSize: 20
+            ),),
+            onTap: () => { Navigator.of(context).pop(),
+//              Navigator.push(
+//                  context, MaterialPageRoute(builder: (context) => ManageAccount()))
+                  },
+          ),
+          ListTile(
+            leading: Icon(Icons.view_headline),
+            title: Text("User's view",style: const TextStyle(
+                color: Color.fromRGBO(254, 246, 222, 1),
+                fontSize: 20
+            ),),
+            onTap: () => {
+              Navigator.of(context).pop(),
+////              Navigator.push(
+//                  context, MaterialPageRoute(builder: (context) => ATMLocator())
+//              )
+        },
+
+          ),
+
+          ListTile(
+            leading: Icon(Icons.local_phone),
+            title: Text('Contact Us' , style: const TextStyle(
+                color: Color.fromRGBO(254, 246, 222, 1),
+                fontSize: 20
+            ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+//              Navigator.push(
+//                  context, MaterialPageRoute(builder: (context) => ContactUs()));
+            },
+
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout',style: const TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontSize: 20
+            ),),
+            onTap: () => {
+    Navigator.of(context).pop(),
+//              Navigator.push(
+//                context, MaterialPageRoute(builder: (context) => Login())
+    })
+            ],
+          ),
+
+      )
+
+    );
+  }
+}
